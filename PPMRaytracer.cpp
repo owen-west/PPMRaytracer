33244ecbe6221c318ad5569cfd9f0115e03e378a
Owen West <west.owen001@gmail.com>
2023-10-10 11:08:59 -0400

    added classes (hittable, hittable_list, camera, utilities, interval, sphere)
    reorganized code into separate classes and added more features within each class, allows spheres to be properly rendered

--- PPMRaytracer.cpp
+++ PPMRaytracer.cpp
@@ -1,29 +1,24 @@
-﻿#include "PPMRaytracer.h"
-#include "color.h"
-#include "vec3.h"
+﻿#include "utilities.h"
+
+#include "camera.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
-int main()
-{
-	// Image Dimensions
-	int image_width = 256;
-	int image_height = 256;
+int main() {
+    hittable_list world;
 
-	// Create PPM image file
-	ofstream outfile("render.ppm", ios_base::out);
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-	// Output to file
-	outfile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    camera cam;
 
-	for (int j = 0; j < image_height; ++j) {
-		std::clog << "\rScanlines remaining: " << (image_height - j - 1) << ' ' << std::flush;
-		for (int i = 0; i < image_width; ++i) {
-			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
-			write_color(outfile, pixel_color);
-		}
-	}
-	std::clog << "\n\rDone.					\n";
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+
+    cam.render(world);
 }
